8333024a05d1c07cd7f4e42c04a25a9c8fd55268
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-30 00:23:37 +0000

    removed users, updated database and server.js

---
+++ db/migrations/20180629235850_initial database creation.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+    knex.schema.createTable('polls', function(table){
+      table.increments();
+      table.string('poll_description');
+      table.string('poll_name');
+    }),
+    knex.schema.createTable('poll_options', function(table){
+      table.increments();
+      table.string('entry_name');
+      table.integer('votes');
+      table.integer('poll_id').unsigned();
+      table.foreign('poll_id').references('polls.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+    knex.schema.dropTable('polls'),
+    knex.schema.dropTable('poll_option')
+  ])
+};
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const PORT          = process.env.PORT || 8080;
 const ENV           = process.env.ENV || "development";
 const express       = require("express");
 const bodyParser    = require("body-parser");
-const cookieSession = require('cookie-session');
-const bcrypt        = require('bcryptjs');
 const sassMiddleware= require("node-sass-middleware");
 const app           = express();
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
+
+
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
@@ -37,23 +37,11 @@ app.use(cookieSession({
 app.use(express.static("public"));
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+
 //===============FUNCTIONS============
-//checks for user
-function currentUser (req) {
-  return req.session.user_id;
-}
-function userInfo (req) {
-  let user = {};
-  knex.select().from("users")
-  .where("id",  req.session.user_id)
-  .then(function (data) {
-    user = data;
-  })
-  .finally(function() {
-    knex.destroy();
-  });
-  return user;
-}
+
+
+
 function generateRandomString() {
   let randomString = "";
   let allPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -61,48 +49,31 @@ function generateRandomString() {
     randomString += allPossibleCharacters.charAt(Math.floor(Math.random() * allPossibleCharacters.length));
   return randomString;
 }
+
 //===============GETS===============
+
 // Home page
 app.get("/", (req, res) => {
-  // let userPresent = currentUser(req);
-  // if(userPresent) {
-  //   res.redirect('/polls_index');
-  // } else {
     res.render("home");
-  // }
 });
+
+
 // Polls index page
 app.get("/polls", (req, res) => {
-//   let userPresent = currentUser(req);
-//   if(userPresent) {
-//       let user = userInfo(req);
-//       templateVars = {
-//         "user": user
-//       };
-//     res.render("polls_index", templateVars);
-//   } else {
-//     res.redirect("/home");
-//   }
-res.render("polls_index")
+  // let templateVars = {
+  // };
+  res.render("polls_index");
 });
+
+
 // // Create poll page
 app.get("/polls/new", (req, res) => {
-//   let userPresent = currentUser(req);
-//   if(userPresent) {
-//       let user = userInfo(req);
-//       templateVars = {
-//         "user": user
-//       };
-//     res.render("poll_new", templateVars);
-//   } else {
-//     res.redirect("/home");
-//   }
   res.render("polls_new");
 });
+
 // Specific poll ID page
 app.get("/polls/:id", (req, res) => {
   // console.log(req.body);
-  // let userPresent = currentUser(req);
   // // Selecting poll options from database
   // let pollOptions = {};
   // knex.select()
@@ -115,48 +86,16 @@ app.get("/polls/:id", (req, res) => {
   //       knex.destroy();
   //     });
   // console.log(pollOptions);
-  // if (userPresent) {
-  //   let user = userInfo();
-  //   templateVars = {
-  //     "user": user,
-  //     "poll_options": pollOptions
-  //   };
-  //   res.render("polls_show", templateVars);
-  // } else {
-  //   templateVars = {
-  //     "poll_options": pollOptions
-  //   };
-  //   res.render("polls_show", templateVars);
-  // }
-  res.render("polls_show");
+  // let user = userInfo();
+  // templateVars = {
+  //   "poll_options": pollOptions
+  // };
+  res.render("polls_show", templateVars);
 });
-// Registration page
-app.get("/registration", (req, res) => {
-  // let userPresent = currentUser(req);
-  // if(userPresent) {
-  //     let user = userInfo(req);
-  //     templateVars = {
-  //       "user": user
-  //     };
-  //   res.render("register", templateVars);
-  // } else {
-    res.render("register");
-  // }
-});
-// Login page
-app.get("/login", (req, res) => {
-  // let userPresent = currentUser(req);
-  // if(userPresent) {
-  //     let user = userInfo(req);
-  //     templateVars = {
-  //       "user": user
-  //     };
-  //   res.render("login", templateVars);
-  // } else {
-    res.render("login");
-  // }
-});
+
+
 //===============POSTS===============
+
 // New poll post
 app.post("/polls", (req, res) => {
   // console.log(req.body);
@@ -177,55 +116,6 @@ app.post("/polls", (req, res) => {
   // .insert({entry_name: , votes: 0, poll_id: pollID});
   res.redirect("/polls/:id");
 });
-// Login post
-app.post("/login", (req, res) => {
-  let emailCheck = req.body.email;
-  let passwordCheck = req.body.password;
-  let user = {};
-
-  knex.select().from('users')
-                  .where('email', req.body.email)
-                  .then(function (data) {
-                    console.log(data);
-                    user = data;
-                  })
-                  .finally(function() {
-                    knex.destroy();
-                  });
-  console.log(user);
-
-//email/password match check
-// if(emailCheck === users[checkUser]["email"] && bcrypt.compareSync(passwordCheck, users[checkUser]["password"]))
-// req.session.user_id = users[checkUser]["cookie_id"];
-// res.redirect("/polls_index");
-
-});
-
-// Registration post
-app.post("/registration", (req, res) => {
-  let cookieID = generateRandomString();
-  let password = req.body.password;
-  let hashedPassword = bcrypt.hashSync(password, 10);
-  let userName = req.body.username;
-  let userEmail = req.body.email;
-
-  knex('users')
-  .insert({
-    username: userName,
-    email: userEmail,
-    password: hashedPassword})
-  .return('id')
-  .then((id) => {
-    req.session.user_id = id;
-    res.status(201).send();
-  });
-});
-
-// Logout post
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/home");
-});
 
 // Specific poll ID DELETE
 app.delete("/polls/:id", (req, res) => {
@@ -237,6 +127,7 @@ app.delete("/polls/:id", (req, res) => {
       knex.destroy();
     });
 });
+
 // Edit poll?
 // app.put("/polls/:id", (req, res) => {
 // });
