19c1b4fd956efb862f93f8ca254d6ac90bff2fc7
Philippe Bolduc <pbolduc2354@gmail.com>
2018-06-30 21:00:03 +0000

    mailgun works for the user that are choose to respond to the poll

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,10 +29,17 @@ $('.fa-plus-square').on('click', event => {
 $('#formSubmit').on('click', event => {
   event.preventDefault();
   let $valueOfFormFields = $('.formData').serializeArray();
+    let $valueOfEmailFields = $('#example_emailBS').val();
+ let temp = JSON.parse($valueOfEmailFields)
+
+  let output = {
+    'form': $valueOfFormFields,
+    'emails': temp
+  }
   $.ajax({
     url: '/polls',
     method: 'POST',
-    data: $valueOfFormFields,
+    data: output,
     success: function (response) {
       console.log('working!')
     }
